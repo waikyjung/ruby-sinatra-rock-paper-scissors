8fae0d326ca56bfa0fd592daad5011dfbe2fde85
waikyjung <waiky.jung@gmail.com>
2024-10-16 16:32:43 +0000

    working

--- app.rb
+++ app.rb
@@ -7,3 +7,17 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome = "tie"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  elsif @comp_move == "scissors"
+    @outcome = "won"
+  end
+  erb(:rock)
+end
+
