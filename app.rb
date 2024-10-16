4e09b0feb9cec78ee3d2707c343da554fa44701a
waikyjung <waiky.jung@gmail.com>
2024-10-16 23:49:41 +0000

    working

--- app.rb
+++ app.rb
@@ -2,22 +2,49 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+def rps(move)
+  comp_move = moves = ["rock", "paper", "scissors"].sample
+  if move == comp_move
+    outcome = "tie"
+  elsif move == "rock" && comp_move == "scissors" 
+    outcome = "won"
+  elsif move == "rock" && comp_move == "paper"
+    outcome = "lost"
+  elsif move == "paper" && comp_move == "rock"
+    outcome = "win"
+  elsif move == "paper" && comp_move == "scissors"
+    outcome = "lost"
+  elsif move == "scissors" && comp_move == "paper"
+    outcome = "win"
+  elsif move == "scissors" && comp_move == "rock"
+    outcome = "lost"
+  end
+  return [comp_move, outcome]
 end
 
 get("/rock") do
-  moves = ["rock", "paper", "scissors"]
-  @comp_move = moves.sample
-  if @comp_move == "rock"
-    @outcome = "tie"
-  elsif @comp_move == "paper"
-    @outcome = "lost"
-  elsif @comp_move == "scissors"
-    @outcome = "won"
-  end
-  erb(:rock)
+  @user_move = "rock"
+  game = rps(@user_move)
+  @comp_move = game[0]
+  @outcome = game[1]
+  erb(:results)
 end
 
+get("/paper") do
+  @user_move = "paper"
+  game = rps(@user_move)
+  @comp_move = game[0]
+  @outcome = game[1]
+  erb(:results)
+end
+
+get("/scissors") do
+  @user_move = "scissors"
+  game = rps(@user_move)
+  @comp_move = game[0]
+  @outcome = game[1]
+  erb(:results)
+end
